21de830fd2b3d55a70f377ea9d62d139331f3fb2
alexzw <alexzw@umich.edu>
2019-09-09 15:43:55 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+x = randint(1, 100)
+b = randint(1, 100)
+sum = x + y
+
+print ("Alice")
+print (a)
+print (b)
+print ("Sum = " + str(sum))
+print ("Average = " + str(sum / 2.0))
